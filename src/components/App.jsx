6326e91a781ac2d34d51574ab729a9dbcfaa0987
Nepalcem <mark.emailtest@gmail.com>
2023-06-16 01:16:39 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { nanoid } from 'nanoid';
 import Form from './Form/Form';
 import ContactsList from './contactsList/ContactsList';
 import Filter from './Filter/Filter';
 import { useSelector, useDispatch } from 'react-redux';
-import { addContact, deleteContact, filterItems } from 'redux/store';
+import { filterItems } from 'redux/slices/filterSlice';
+import { addContact, deleteContact } from 'redux/slices/contactsSlice';
 
 const App = () => {
   const contacts = useSelector(state => state.contacts);
   const filter = useSelector(state => state.filter);
   const dispatch = useDispatch();
-
-  // const [contacts, setContacts] = useState(
-  //   JSON.parse(localStorage.getItem('contacts')) ?? [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ]
-  // );
 
   const addContactItem = ({ name, number }) => {
     if (
@@ -50,10 +42,6 @@ const App = () => {
   const removeContact = id => {
     dispatch(deleteContact(id));
   };
-
-  useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
 
   return (
     <div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,38 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
-// import { createReducer } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { contactsSlice } from './slices/contactsSlice';
+import { filterSlice } from './slices/filterSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage: storage,
+};
 
-const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState: contactsInitialState,
-  reducers: {
-    addContact(state, action) {
-      state.push(action.payload);
-    },
-    deleteContact(state, action) {
-    return state.filter(contact => contact.id !== action.payload)
-    },
-  }
+const rootReducer = combineReducers({
+  contacts: contactsSlice.reducer,
+  filter: filterSlice.reducer,
 });
 
-export const {addContact, deleteContact} = contactsSlice.actions;
-
-const filterSlice = createSlice({
-  name: 'filter',
-  initialState: '',
-  reducers: {
-    filterItems(state, action) {return action.payload }
-  }
-});
-
-export const { filterItems} = filterSlice.actions;
-
-
+const persistedReducer = persistReducer(contactsPersistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { contacts: contactsSlice.reducer, filter: filterSlice.reducer },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
